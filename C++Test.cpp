d2a63b757300e3c46fbd70d7066a4392b2b680f2
admin <admin@DESKTOP-S37LMTL>
2024-06-19 14:47:19 +0800

    添加项目文件。

---
+++ C++Test.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template<class T>
+class A {
+public:
+	T a;
+	A(T a) : a(a) {}
+	A() {}
+	T fun(T a);
+	void print();
+};
+
+class B :public A<int> {
+
+};
+
+
+
+int main() {
+
+	A<int> a(5);
+	a.print();
+
+}
+
+template<typename T>
+T A<T>::fun(T a) {
+	return a;
+}
+
+template<class T>
+void A<T>::print() {
+	cout << a << endl;
+}
